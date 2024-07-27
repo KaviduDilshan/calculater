8f78a5352c6580e952e32e3d73b8e61223a6dc0e
KaviduDilshan <dilshank404@gmail.com>
2024-07-27 15:52:22 +0530

    create js file and access to input and calculate parts

---
+++ index.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+
+function appendToDisply(input){
+    display.value += input;
+}
+
+function clearDisplay(input){
+    display.value =  " " ;
+}
+
+function calculate(input){
+    try{
+        display.value =  eval(display.value);
+    }
+
+    catch(error){
+        display.value = "error";
+    }
+}
